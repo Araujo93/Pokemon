ce57933198bcc33f5c06cc74b1527b190382756e
Sean Araujo <seanaraujo93@gmail.com>
2022-04-19 14:13:13 -0400

    Removed similar files and made one component to render all pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import Navbar from "./components/navbar/Navbar";
 import PokemonList from "./components/pokemon/pokeList/PokemonList";
 import { Route, Switch } from "react-router-dom";
 import PokeInfo from "./components/pokemon/pokeInfo/PokeInfo";
 import RegisterUser from "./components/auth/RegisterUser";
 import Home from "./components/home/Home";
-import SecondGenPokemon from "./components/pokemon/SecondGenPokemon";
-import ThirdGenPokemon from "./components/pokemon/ThirdGenPokemon";
+import Pokemon from "./components/pokemon/Pokemon";
 import { BrowserRouter as Router } from "react-router-dom";
+import { IPokemon } from "./interfaces/interfaces";
 
 import LoginUser from "./components/auth/LoginUser";
 
 function App(): JSX.Element {
+  const [secondGenPokemon, setSecondGenPokemon] = useState<IPokemon[]>([]);
+  const [thirdGenPokemon, setThirdGenPokemon] = useState<IPokemon[]>([]);
+
+  useEffect(() => {
+    const getSecondGenPokemon = async () => {
+      const res = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?offset=151&limit=100"
+      );
+      const data = await res.json();
+      setSecondGenPokemon(data.results);
+    };
+    const getThirdGenPokemon = async () => {
+      const res = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?offset=251&limit=135"
+      );
+      const data = await res.json();
+      setThirdGenPokemon(data.results);
+    };
+    getThirdGenPokemon();
+    getSecondGenPokemon();
+  }, [secondGenPokemon, thirdGenPokemon]);
+
   return (
     <div className="App">
       <Router>
@@ -24,8 +47,14 @@ function App(): JSX.Element {
             <Route path="/first" component={PokemonList} />
             <Route path="/home" component={Home} />
             <Route path="/about/:id" component={PokeInfo} />
-            <Route path="/secondGen" component={SecondGenPokemon} />
-            <Route path="/thirdGen" component={ThirdGenPokemon} />
+            <Route
+              path="/secondGen"
+              render={() => <Pokemon item={secondGenPokemon} />}
+            />
+            <Route
+              path="/thirdGen"
+              render={() => <Pokemon item={thirdGenPokemon} />}
+            />
           </>
         </Switch>
       </Router>
---
+++ src/components/pokemon/Pokemon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PokeCard from "./pokeCard/PokeCard";
+import { IPokemon } from "../../interfaces/interfaces";
+import "./pokeList/pokeList.css";
+
+const Pokemon = (props: { item: IPokemon[] }) => {
+  return (
+    <>
+      <div className="pokemonList">
+        {props.item.map((item: IPokemon, index) => (
+          <PokeCard key={index} item={item} />
+        ))}
+      </div>
+      <h3 onClick={() => (document.documentElement.scrollTop = 0)}>
+        Back to top
+      </h3>
+    </>
+  );
+};
+
+export default Pokemon;
